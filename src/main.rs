00a3780051849e331815efdb7bfcb366a8674752
Filip Gospodinov <f@gospodinov.ch>
2020-03-13 09:18:26 +0100

    use bitcoin::BlockHeader in API
    This simplifies the function signatures while
    increasing the readability of the tests.

--- src/main.rs
+++ src/main.rs
@@ -14,55 +14,22 @@ pub fn rust_neoscrypt(message: Vec<u8>, options: u32) -> bitcoin::util::uint::Ui
     bitcoin::consensus::encode::deserialize(&buf).unwrap()
 }
 
-fn mine(
-    nVersion: u32,
-    hashPrevBlock: &str,
-    hashMerkleRoot: &str,
-    nTime: u32,
-    nBits: u32,
-    initialNonce: Option<u32>,
-) -> Option<u32> {
+fn mine(mut header: bitcoin::BlockHeader, initialNonce: Option<u32>) -> Option<u32> {
     let lower = match initialNonce {
         None => 0,
         Some(i) => i,
     };
     for nNonce in lower..u32::max_value() {
         println!("{:?}", nNonce);
-        if check_hash(
-            nVersion,
-            hashPrevBlock,
-            hashMerkleRoot,
-            nTime,
-            nBits,
-            nNonce,
-        ) {
+        header.nonce = nNonce;
+        if check_hash(header) {
             return Some(nNonce);
         }
     }
     None
 }
 
-fn check_hash(
-    nVersion: u32,
-    hashPrevBlock: &str,
-    hashMerkleRoot: &str,
-    nTime: u32,
-    nBits: u32,
-    nNonce: u32,
-) -> bool {
-    let mut prev_block = Vec::from_hex(hashPrevBlock).unwrap();
-    prev_block.reverse();
-    let mut merkle_root = Vec::from_hex(hashMerkleRoot).unwrap();
-    merkle_root.reverse();
-
-    let header = bitcoin::BlockHeader {
-        version: nVersion,
-        prev_blockhash: bitcoin::consensus::encode::deserialize(&prev_block).unwrap(),
-        merkle_root: bitcoin::consensus::encode::deserialize(&merkle_root).unwrap(),
-        time: nTime,
-        bits: nBits,
-        nonce: nNonce,
-    };
+fn check_hash(header: bitcoin::BlockHeader) -> bool {
     let neo_scrypt_options: u32 = 0x1000;
     rust_neoscrypt(
         bitcoin::consensus::encode::serialize(&header),
@@ -73,26 +40,33 @@ fn check_hash(
 fn main() {}
 
 #[cfg(test)]
+
+fn into<T: bitcoin::consensus::encode::Decodable>(hex_string: &str) -> T {
+    let mut bytes = Vec::from_hex(hex_string).unwrap();
+    bytes.reverse();
+    bitcoin::consensus::encode::deserialize(&bytes).unwrap()
+}
+
 #[test]
 fn test_real_blocks() {
     // 277930
-    assert!(check_hash(
-        536870912,
-        "508ff5554dc9f83e3cab31b8fb89d10604ebcd963c7b8ce131fd3ef47c2a0921",
-        "a86996c4ae298b22a4a1c971aa29f0ea9fb1d822191fa8e0755b08381a90bc19",
-        1583516502,
-        0x1d01cc13,
-        867267585,
-    ));
+    assert!(check_hash(bitcoin::BlockHeader {
+        version: 536870912,
+        prev_blockhash: into("508ff5554dc9f83e3cab31b8fb89d10604ebcd963c7b8ce131fd3ef47c2a0921"),
+        merkle_root: into("a86996c4ae298b22a4a1c971aa29f0ea9fb1d822191fa8e0755b08381a90bc19"),
+        time: 1583516502,
+        bits: 0x1d01cc13,
+        nonce: 867267585,
+    }));
     // 277960
-    assert!(check_hash(
-        536870912,
-        "4cb8068ec9d224d5869fe57144cff796e4c88f9da5ebe81dab88cd247d93e0e2",
-        "3d976f66323f5d79880c8c87bcd35506f43e27c9ec75468572db57aacf7c1b72",
-        1583519758,
-        0x1d023fa9,
-        2041135619,
-    ));
+    assert!(check_hash(bitcoin::BlockHeader {
+        version: 536870912,
+        prev_blockhash: into("4cb8068ec9d224d5869fe57144cff796e4c88f9da5ebe81dab88cd247d93e0e2"),
+        merkle_root: into("3d976f66323f5d79880c8c87bcd35506f43e27c9ec75468572db57aacf7c1b72"),
+        time: 1583519758,
+        bits: 0x1d023fa9,
+        nonce: 2041135619,
+    }));
 }
 
 #[test]
@@ -100,11 +74,18 @@ fn test_mine() {
     // 277930
     assert_eq!(
         mine(
-            536870912,
-            "508ff5554dc9f83e3cab31b8fb89d10604ebcd963c7b8ce131fd3ef47c2a0921",
-            "a86996c4ae298b22a4a1c971aa29f0ea9fb1d822191fa8e0755b08381a90bc19",
-            1583516502,
-            0x1d01cc13,
+            bitcoin::BlockHeader {
+                version: 536870912,
+                prev_blockhash: into(
+                    "508ff5554dc9f83e3cab31b8fb89d10604ebcd963c7b8ce131fd3ef47c2a0921"
+                ),
+                merkle_root: into(
+                    "a86996c4ae298b22a4a1c971aa29f0ea9fb1d822191fa8e0755b08381a90bc19"
+                ),
+                time: 1583516502,
+                bits: 0x1d01cc13,
+                nonce: 0,
+            },
             Some(867267585 - 10),
         ),
         Some(867267585),
@@ -112,13 +93,20 @@ fn test_mine() {
     // 277960
     assert_eq!(
         mine(
-            536870912,
-            "4cb8068ec9d224d5869fe57144cff796e4c88f9da5ebe81dab88cd247d93e0e2",
-            "3d976f66323f5d79880c8c87bcd35506f43e27c9ec75468572db57aacf7c1b72",
-            1583519758,
-            0x1d023fa9,
-            Some(2041135619 - 10),
+            bitcoin::BlockHeader {
+                version: 536870912,
+                prev_blockhash: into(
+                    "4cb8068ec9d224d5869fe57144cff796e4c88f9da5ebe81dab88cd247d93e0e2"
+                ),
+                merkle_root: into(
+                    "3d976f66323f5d79880c8c87bcd35506f43e27c9ec75468572db57aacf7c1b72"
+                ),
+                time: 1583519758,
+                bits: 0x1d023fa9,
+                nonce: 0,
+            },
+            Some(2041135619 - 10)
         ),
-        Some(2041135619),
+        Some(2041135619)
     );
 }
